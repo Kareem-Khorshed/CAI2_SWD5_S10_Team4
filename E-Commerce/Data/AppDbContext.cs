b10aaeb70a65d1778797aa20181586531c5b2e61
Kareem <Kareemkhorshed2021@gmail.com>
2025-04-19 12:07:59 +0200

    First Commit To Add Project Woth -f To Delete The Previous

---
+++ E-Commerce/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using E_Commerce.Models;
+using Microsoft.EntityFrameworkCore;
+namespace E_Commerce.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Users> Users {get ; set;}
+    
+    }
+}
